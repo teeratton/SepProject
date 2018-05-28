1c11107b40d3c4c44d3f14a4df1d84629f45b636
Ton <teerat968@hotmail.com>
2018-05-28 23:56:19 +0700

    hbjhbv

--- loginSystem.py
+++ loginSystem.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'firstLoginPage.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.2
-#
-# WARNING! All changes made in this file will be lost!
-
 from firebase import firebase
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
